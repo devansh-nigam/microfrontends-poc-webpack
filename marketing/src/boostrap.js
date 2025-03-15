ad50817f8449b6ebb2e9ecf86c2af4bf447705f4
devansh-nigam <devanshnigam0003@gmail.com>
2025-03-15 19:36:13 +0530

    Added primereact and related dependenices, added css-loader

--- marketing/src/boostrap.js
+++ marketing/src/boostrap.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { PrimeReactProvider } from "primereact/api";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/mira/theme.css";
 
-import App from "./App.jsx";
+import App from "./App";
 
 const mount = (el) => {
   const root = createRoot(el);
-  root.render(<App />);
+  root.render(
+    <React.StrictMode>
+      <PrimeReactProvider>
+        <App />
+      </PrimeReactProvider>
+    </React.StrictMode>
+  );
 };
 
 if (process.env.NODE_ENV == "development") {
